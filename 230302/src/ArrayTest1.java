f6f3d5850811325d844b5670d601f9dca1dcae6e
Yebin Ha <beanswebdev@gmail.com>
2023-03-02 10:32:04 +0900

    230302 - 배열, 랜덤 로또번호 얻기

---
+++ 230302/src/ArrayTest1.java
@@ -0,0 +1,45 @@
+
+public class ArrayTest1 {
+	public static void main(String[] args) {
+		/**
+		 * int 배열 생성 & 초기화 
+		 */
+		int[] arr1 = new int[5];
+		arr1[0] = 10;
+		arr1[1] = 20;
+		arr1[2] = 30;
+		arr1[3] = 40;
+		arr1[4] = 50;
+		//arr1[5] = 60; //컴파일 에러 ArrayIndexOutOfBoundsException
+		
+		//배열 생성과 동시 초기화할시 배열의 길이[] 생략해야 함 
+		int[] arr2 = new int[] {10,20,30,40,50};
+		
+		//배열 생성과 동시 초기화시 new int[] 생략 가능 
+		int[] arr4 = {10,20,30,40,50};
+		
+		//배열 선언 후 생성&초기화 시 new int[] 생략 불
+		int[] arr3;
+		arr3 = new int[] {10,20,30,40,50};
+		
+		//컴파일 에러 
+//		int[] arr4;
+//		arr4 = {10,20,30,40,50};
+		
+		int sum = 0;
+		for(int i=0; i<arr2.length; i++) {
+			sum += arr2[i];
+		}
+		System.out.println(sum);
+		
+		int n,m;
+		// 변수 n에 10 담기 
+		n = 10;
+		// 변수 m에 n값 복사 
+		m = n;
+		System.out.println(n);
+		// 리터럴 값은 readonly 
+//		10 = 20;
+	}
+
+}
---
+++ 230302/src/Lotto.java
@@ -0,0 +1,41 @@
+
+public class Lotto {
+	public static void main(String[] args) {
+		
+		//로또번호 6개 
+		int[] winner = new int[6];
+		System.out.print("로또번호: ");
+		
+		for (int i=0; i<winner.length; i++) {
+			int num = (int)(Math.random()*46+1);
+			winner[i] = num;
+			System.out.print(winner[i] +" ");
+		}
+		
+		System.out.println();
+		
+		//배열의 두 인덱스 값을 바꾸는 법 : swap
+		int[] ball = new int[46];
+		for(int i=0; i<ball.length; i++) {
+			ball[i] = i+1;
+		}
+		//1000번 섞기 
+		for(int i=0; i<1000; i++) {
+			int ran1 = (int)(Math.random()*46);
+			int ran2 = (int)(Math.random()*46);
+			
+			if(ran1 != ran2) {
+				int temp = ball[ran1];
+				ball[ran1] = ball[ran2];
+				ball[ran2] = temp;		
+			}
+		}
+		
+		for (int i=0; i<6; i++) {
+			System.out.print(ball[i]+",");
+		}
+		
+		
+		
+	}
+}
