252351b1f4ad85cc24e2542d973fa418b5dd45fd
Yebin Ha <beanswebdev@gmail.com>
2023-03-08 16:37:14 +0900

    230308 - 패키지

---
+++ 230308/src/pac/MyClassTest.java
@@ -0,0 +1,14 @@
+package pac;
+
+import java.sql.Date;
+
+import com.kosta.MyClass;
+
+public class MyClassTest {
+	public static void main(String[] args) {
+		MyClass mc = new MyClass();
+		
+		Date date = new Date(2023, 3, 8);
+		java.util.Date date2 = new java.util.Date();
+	}
+}
