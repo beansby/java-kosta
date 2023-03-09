6f97baec4582a1b49de03bcf89cd3f3941859ecb
Yebin Ha <beanswebdev@gmail.com>
2023-03-09 09:05:18 +0900

    null

--- 230308/src/pac/MyClassTest.java
+++ 230308/src/pac/MyClassTest.java
@@ -1,8 +1,9 @@
 package pac;
 
 import java.sql.Date;
-
-import com.kosta.MyClass;
+//import com.kosta.MyClass;
+//import com.kosta.YourClass;
+import com.kosta.*;	//kosta의 하위 패키지까지 import 되는 것은 아님 
 
 public class MyClassTest {
 	public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class MyClassTest {
 		
 		Date date = new Date(2023, 3, 8);
 		java.util.Date date2 = new java.util.Date();
+		
+		YourClass yc = new YourClass();
 	}
 }
