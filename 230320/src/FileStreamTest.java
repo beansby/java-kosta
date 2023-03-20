a923cc0a89e8181a256516e42c9e9768cd29aae6
Yebin Ha <beanswebdev@gmail.com>
2023-03-20 11:07:26 +0900

    230320 - stream I/O, file copy 추가

--- 230320/src/FileStreamTest.java
+++ 230320/src/FileStreamTest.java
@@ -11,9 +11,17 @@ public class FileStreamTest {
 			fis = new FileInputStream("fileExample.txt");
 			fos = new FileOutputStream("fileCopy.txt");
 			
-			int data;
-			while((data = fis.read()) != -1) {
-				fos.write(data);
+//			int data;
+//			while((data = fis.read()) != -1) {
+//				fos.write(data);
+//			}
+			
+			byte[] buff = new byte[512];
+			int len;
+			// 읽어온 길이가 0보다 크면 
+			while((len = fis.read(buff)) > 0) {
+				//0,len을 생략할경우 buff의 길이를 읽어오게 됨 : buff에 있는 데이터 중 읽어온 0번째부터 ? 까지 
+				fos.write(buff, 0, len);
 			}
 			
 		} catch (IOException e) {
